c7b607a612e05f4718973100d9b60fc1fa8e419a
Larry Boltovskoi <larry.bolt@gmail.com>
2019-05-12 19:29:57 +0100

    Fix eslint warnings

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-import { ReactiveBase, DataSearch, ReactiveList } from '@appbaseio/reactivesearch';
+import { DataSearch, ReactiveBase, ReactiveList } from '@appbaseio/reactivesearch';
 import Result from './result';
 
-class App extends Component {
-  render() {
-    return (
-      <ReactiveBase app="amplyfi" url={process.env.REACT_APP_SEARCH_API}>
-        <div>
-          <DataSearch
-            componentId="search"
-            dataField={[
-						  'm_Companies',
-						  'm_BiGrams',
-						  'm_szDocTitle',
-						  'm_People',
-						  'm_Places',
-						  'm_TriGrams',
-            ]}
-          />
-        </div>
-        <ReactiveList
-          componentId="SearchResult"
-          dataField="m_szDocID"
-          react={{
-					  and: ['search'],
-          }}
-          includeFields={['m_szDocID', 'm_szDocTitle', 'm_szSrcUrl']}
-          pagination
-          renderItem={res => <Result key={res.m_szDocID} {...res} />}
+function App() {
+  return (
+    <ReactiveBase app="amplyfi" url={process.env.REACT_APP_SEARCH_API}>
+      <div>
+        <DataSearch
+          componentId="search"
+          dataField={[
+            'm_Companies',
+            'm_BiGrams',
+            'm_szDocTitle',
+            'm_People',
+            'm_Places',
+            'm_TriGrams',
+          ]}
         />
-      </ReactiveBase>
-    );
-  }
+      </div>
+      <ReactiveList
+        componentId="SearchResult"
+        dataField="m_szDocID"
+        react={{
+          and: ['search'],
+        }}
+        includeFields={['m_szDocID', 'm_szDocTitle', 'm_szSrcUrl']}
+        pagination
+        renderItem={res => <Result key={res.m_szDocID} {...res} />}
+      />
+    </ReactiveBase>
+  );
 }
+
 export default App;
--- src/result.js
+++ src/result.js
@@ -1,20 +1,25 @@
-import React, { Component } from 'react';
+/* eslint-disable camelcase */
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class Result extends Component {
-  render() {
-    return (
-      <div>
-        <p>
-          {this.props.m_szDocTitle}
-          <small>
-                        (
-            <a href={this.props.m_szSrcUrl}>source</a>
-)
-          </small>
-        </p>
-        <p>{this.props.m_szDocSumamry}</p>
-      </div>
-    );
-  }
+function Result(props) {
+  const { m_szDocTitle, m_szSrcUrl, m_szDocSumamry } = props;
+  return (
+    <div>
+      <p>
+        {m_szDocTitle}
+        <small>
+          <a href={m_szSrcUrl}>source</a>
+        </small>
+      </p>
+      <p>{m_szDocSumamry}</p>
+    </div>
+  );
 }
+Result.propTypes = {
+  m_szDocTitle: PropTypes.string.isRequired,
+  m_szSrcUrl: PropTypes.string.isRequired,
+  m_szDocSumamry: PropTypes.string.isRequired,
+};
+
 export default Result;
