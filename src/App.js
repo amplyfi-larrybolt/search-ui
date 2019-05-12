bccc78be5d139307ea5e9adeeac5e01999eda2fe
Larry Boltovskoi <larry.bolt@gmail.com>
2019-05-12 21:06:19 +0100

    Style results a little bit and only show when searched

--- src/App.js
+++ src/App.js
@@ -1,37 +1,73 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-import { DataSearch, ReactiveBase, ReactiveList } from '@appbaseio/reactivesearch';
+import {DataSearch, ReactiveBase, ReactiveList} from '@appbaseio/reactivesearch';
+
 import Result from './result';
 
-function App() {
-  return (
-    <ReactiveBase app="amplyfi" url={process.env.REACT_APP_SEARCH_API}>
-      <div>
-        <DataSearch
-          componentId="search"
-          dataField={[
-            'm_Companies',
-            'm_BiGrams',
-            'm_szDocTitle',
-            'm_People',
-            'm_Places',
-            'm_TriGrams',
-          ]}
-        />
-      </div>
-      <ReactiveList
-        componentId="SearchResult"
-        dataField="m_szDocID"
-        react={{
-          and: ['search'],
-        }}
-        includeFields={['m_szDocID', 'm_szDocTitle', 'm_szSrcUrl']}
-        pagination
-        renderItem={res => <Result key={res.m_szDocID} {...res} />}
-      />
-    </ReactiveBase>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchQuery: '',
+    };
+    this.searchChange = this.searchChange.bind(this);
+  }
+
+  searchChange(value) {
+    this.setState({
+      searchQuery: value,
+    });
+  }
+
+  render() {
+    return (
+      <Container fluid>
+        <ReactiveBase app="amplyfi" url={process.env.REACT_APP_SEARCH_API}>
+          <Row>
+            <Col sm={8}>
+              <DataSearch
+                componentId="search"
+                dataField={[
+                  'm_Companies',
+                  'm_BiGrams',
+                  'm_szDocTitle',
+                  'm_People',
+                  'm_Places',
+                  'm_TriGrams',
+                ]}
+                value={this.state.searchQuery}
+                onChange={this.searchChange}
+                debounce={200}
+              />
+            </Col>
+            <Col sm={2}>
+              Amplyfi Article Search
+            </Col>
+          </Row>
+          {this.state.searchQuery.length > 0 && (
+            <Row>
+              <Col>
+                <ReactiveList
+                  componentId="SearchResult"
+                  dataField="m_szDocID"
+                  react={{
+                    and: ['search'],
+                  }}
+                  includeFields={['m_szDocID', 'm_szDocTitle', 'm_szSrcUrl']}
+                  pagination
+                  renderItem={res => <Result key={res.m_szDocID} {...res} />}
+                />
+              </Col>
+            </Row>
+          )}
+        </ReactiveBase>
+      </Container>
+    );
+  }
 }
 
 export default App;
--- src/result.js
+++ src/result.js
@@ -3,23 +3,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Result(props) {
-  const { m_szDocTitle, m_szSrcUrl, m_szDocSumamry } = props;
+  const { m_szDocTitle, m_szSrcUrl } = props;
   return (
-    <div>
-      <p>
+    <div className="link">
+      <p className="title">
         {m_szDocTitle}
-        <small>
-          <a href={m_szSrcUrl}>source</a>
-        </small>
       </p>
-      <p>{m_szDocSumamry}</p>
+      <small>
+        <a href={m_szSrcUrl}>source</a>
+      </small>
     </div>
   );
 }
 Result.propTypes = {
   m_szDocTitle: PropTypes.string.isRequired,
   m_szSrcUrl: PropTypes.string.isRequired,
-  m_szDocSumamry: PropTypes.string.isRequired,
 };
 
 export default Result;
