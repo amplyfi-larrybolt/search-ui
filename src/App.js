00c5eb0ca63e35f2e69acf2699868f6bb0bba129
Larry Boltovskoi <larry.bolt@gmail.com>
2019-05-12 19:06:44 +0100

    Add eslint and airbnb style

--- src/App.js
+++ src/App.js
@@ -5,35 +5,34 @@ import { ReactiveBase, DataSearch, ReactiveList } from '@appbaseio/reactivesearc
 import Result from './result';
 
 class App extends Component {
-
-	render() {
-		return (
-			<ReactiveBase app="amplyfi" url={process.env.REACT_APP_SEARCH_API}>
-				<div>
-					<DataSearch
-						componentId="search"
-						dataField={[
-							"m_Companies",
-							'm_BiGrams',
-                            'm_szDocTitle',
-							'm_People',
-							'm_Places',
-							'm_TriGrams',
-						]}
-					/>
-				</div>
-                <ReactiveList
-					componentId="SearchResult"
-					dataField="m_szDocID"
-					react={{
-						"and": ["search"]
-					}}
-                    includeFields={['m_szDocID', 'm_szDocTitle', 'm_szSrcUrl']}
-                    pagination={true}
-					renderItem={(res) => <Result key={res.m_szDocID} {...res}/>}
-				/>
-			</ReactiveBase>
-		);
-	}
+  render() {
+    return (
+      <ReactiveBase app="amplyfi" url={process.env.REACT_APP_SEARCH_API}>
+        <div>
+          <DataSearch
+            componentId="search"
+            dataField={[
+						  'm_Companies',
+						  'm_BiGrams',
+						  'm_szDocTitle',
+						  'm_People',
+						  'm_Places',
+						  'm_TriGrams',
+            ]}
+          />
+        </div>
+        <ReactiveList
+          componentId="SearchResult"
+          dataField="m_szDocID"
+          react={{
+					  and: ['search'],
+          }}
+          includeFields={['m_szDocID', 'm_szDocTitle', 'm_szSrcUrl']}
+          pagination
+          renderItem={res => <Result key={res.m_szDocID} {...res} />}
+        />
+      </ReactiveBase>
+    );
+  }
 }
 export default App;
--- src/result.js
+++ src/result.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 
 class Result extends Component {
-
-    render() {
-        return (
-            <div>
-                <p>
-                    {this.props.m_szDocTitle}
-                    <small>
-                        (<a href={this.props.m_szSrcUrl}>source</a>)
-                    </small>
-                </p>
-                <p>{this.props.m_szDocSumamry}</p>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <p>
+          {this.props.m_szDocTitle}
+          <small>
+                        (
+            <a href={this.props.m_szSrcUrl}>source</a>
+)
+          </small>
+        </p>
+        <p>{this.props.m_szDocSumamry}</p>
+      </div>
+    );
+  }
 }
 export default Result;
